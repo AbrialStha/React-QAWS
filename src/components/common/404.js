f6ec2533a7faa683d2f331eea4d2d4ab1ce769c5
Abiral <abi.sthapit@gmail.com>
2018-01-10 03:09:20 +0545

    added 404 component

--- src/components/common/404.js
+++ src/components/common/404.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import pic from '../../Assets/404-venn.jpg'
 
 class PageNotFound extends Component {
+    goBack = () => {
+        this.props.history.goBack()
+    }
     render() {
         return (
-            <div>
-                Page Not Found
+            <div className="card page_not_found">
+                <img alt="404-venn" src={pic} width='300' />
+                <div className="card-block">
+                    <h3 className="card-title">Page Not Found<hr /></h3>
+                    <p className="card-text justify-content-end">
+                        The page you are looking for doesn't exist or some other error occured.
+                        <br /><br />
+                        <button className="btn btn-raised btn-primary" onClick={this.goBack}>Go Back</button>
+                    </p>
+                </div>
             </div>
         )
     }
