6469784f1911715b58ff3927ee37c62ecd63b60f
Abiral <abi.sthapit@gmail.com>
2018-01-13 18:10:19 +0545

    append flashmessage to root reducer

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import todo from './Todo'
 import assigned from './assigned'
+import flash_messages from './FlashMessage'
 
 export default combineReducers({
     auth,
     todo,
-    assigned
+    assigned,
+    flash_messages
 })
