673aeb9bac5800eb1934567ef485c6150efd2a6c
Abiral <abi.sthapit@gmail.com>
2018-01-13 18:12:11 +0545

    Added the toaster notification component in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navigation from './common/Navigation'
+import Toaster from './toaster/toaster'
 
 class Main extends Component {
     render() {
         return (
             <div>
                 <div className="area">
+                <Toaster />
                     {this.props.children}
                 </div>
                 <Navigation />
